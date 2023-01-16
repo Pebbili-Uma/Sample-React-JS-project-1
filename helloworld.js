ae7eb6c9632b3c3469567df9b2ddb572e4e0c092
Pebbili-Uma <75122885+Pebbili-Uma@users.noreply.github.com>
2023-01-16 14:39:51 +0530

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,8 @@
-.greeting {
-  color: green;
-}
+let rootEl = document.getElementById("root");
+const element = (
+  <div>
+    <h1 className="greeting"> Hello </h1>
+    <p> Good to see you! </p>
+  </div>
+);
+ReactDOM.render(element, rootEl);
